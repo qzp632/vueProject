18183232ddff9d1f7441f6802025ffa1d16ace87
v_qinzp <v_qinzp@fosun.com>
2018-07-12 14:34:58 +0800

    'update'

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created () {
+    fetch('v2/book/1220562', {
+        method: 'GET'
+      })
+      .then((res)=>{
+        return res.text()
+      })
+      .then((res)=>{
+        console.log(res)
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
